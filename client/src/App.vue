498fdc7ab0ae1a13dca4dc00a99281129d9ff2f8
nasriime <mnasr4040@gmail.com>
2022-01-19 21:57:01 +0100

    add style for take note input

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,40 +3,50 @@
     div.search
       img(
         src="./assets/images/search-icon.png"
-        class="search--img"
+        class="search__img"
       )
       input(
         type='text'
         name='search'
-        class="search--input"
+        class="search__input"
         placeholder='Search'
         @input="search"
       )
-    input(
-      type='text'
-      name='take_note'
-      placeholder='Take a note'
-      v-model="item"
-    )
-    button(type='button' @click="addItem") +
-    ListItem(
-      v-for="(item, index) in data"
-      :item="item"
-      :key="item._id"
-      @deleted-item="deleteItem"
-      @edited-item="updateItem"
-    )
-    div(v-if="data.length === 0") No results found
-    button(
-      type='button'
-      name="previous_button"
-      :disabled="!hasPrevPage"
-      @click="navigate('previous')") <
-    button(
-      type='button'
-      name="next_button"
-      :disabled="!hasNextPage"
-      @click="navigate('next')") >
+    div.list
+      div.note
+        img(
+          src="./assets/images/Add.png"
+          class="note__img"
+          @click="addItem"
+        )
+        input(
+          type='text'
+          name='take_note'
+          class="note__input"
+          placeholder='Take a note'
+          v-model="item"
+        )
+      ListItem(
+        v-for="(item, index) in data"
+        :item="item"
+        :key="item._id"
+        @deleted-item="deleteItem"
+        @edited-item="updateItem"
+      )
+      div(v-if="data.length === 0") No results found
+      div.buttons--wrapper
+        button(
+          type='button'
+          name="previous_button"
+          :disabled="!hasPrevPage"
+          @click="navigate('previous')"
+          ) <
+        button(
+          type='button'
+          name="next_button"
+          :disabled="!hasNextPage"
+          @click="navigate('next')"
+          ) >
 </template>
 
 <script lang="ts">
@@ -79,6 +89,9 @@ export default defineComponent({
 <style lang="scss">
 @import "./assets/scss/_variables.scss";
 
+*{
+  box-sizing: border-box;
+}
 body{
   background: $bg;
 }
@@ -89,12 +102,12 @@ body{
 .search{
   margin-bottom: 20px;
   position: relative;
-  &--img{
+  &__img{
     position: absolute;
     top: 10px;
     left: 10px;
   }
-  &--input{
+  &__input{
     border-radius: 12px;
     border-color: transparent;
     width: 100%;
@@ -104,5 +117,30 @@ body{
     font-size: 16px;
     color: #787878;
   }
+}
+.list{
+  width: 100%;
+}
+.note{
+    position: relative;
+    border-bottom: 1px solid #C6C6C6;
+  &__img{
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    cursor: pointer;
+  }
+  &__input{
+    width: 100%;
+    padding: 10px;
+    border: none;
+    outline: none;
+    color: #A8A8A8;
+    font-size: 16px;
+  }
+}
+
+.buttons__wrapper{
+  text-align: right;
 }
 </style>
